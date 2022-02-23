ebf48f1a2a027066b4c80461eb157c933e981188
Xin Guan <guanxin2012@gmail.com>
2022-02-22 21:28:20 -0800

    Add comments, and modify methods in Node.

--- src/com/company/Message.java
+++ src/com/company/Message.java
@@ -7,25 +7,31 @@ public class Message {
   enum MessageType {
     FIND_SUCCESSOR, RETURN_PREDECESSOR, NOTIFY, PING
   }
-
-  public static int requestFindSuccessor(int id, Node targetNode) {
-
+  
+  // Ask targetNode to run findSuccessor(id), return the successor node
+  public static Node requestFindSuccessor(int id, Node targetNode) {
+    return null;
   }
 
-  public static int requestReturnPrecessor(Node targetNode) {
-
+  // Ask targetNode to return its predecessor, return the predecessor node
+  public static Node requestReturnPredecessor(Node targetNode) {
+    return null;
   }
 
+  // Ask targetNode to run notify(selfNode)
   public static void requestNotify(Node selfNode, Node targetNode) {
 
   }
 
+  // Ping targetNode to see if it is alive (return 0: OK. otherwise: failed)
   public static int requestPing(Node targetNode) {
-
+    return 0;
   }
 
+  // Receive one incoming message, parse it, and run the corresponding method on selfNode 
+  // (return 0: OK. otherwise: failed)
   public static int receiveIncomingMessage(DatagramSocket socket, Node selfNode) {
-
+    return 0;
   }
 
 }
